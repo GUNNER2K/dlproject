62e940d65d5501b4fdcfbcceab56e7057d9d2e9b
Satyaki <satyakibhattacharjee252@gmail.com>
2023-06-25 19:16:11 +0530

    third commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # dlproject
+
+
+## Workflow 
+
+1. Update config.yaml
+2. Update secrets.yaml
+3. Update params.yamal
+4. Update the entity 
+5. Update Configuration Manager
+6. Update the components
+7. Update the pipeline
+8. Update the main.py
+9. Update DVC.yaml
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src import logger
 from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion stage'
 try :
@@ -10,3 +11,15 @@ try :
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = 'Prepare base Model'
+try :
+    logger.info(f">>>>>>>> stage {STAGE_NAME} STARTED<<<<<<<<<<")
+    data_ingestion = PrepareBaseModelTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>>>{STAGE_NAME} COMPLETED<<<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e 
